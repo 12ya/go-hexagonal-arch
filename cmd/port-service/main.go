b84732f0e34a7675f8ce491aca28bb9f30425d09
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2026-01-20 21:17:28 +0900

    cmd/port-service/main: handle syscalls for graceful shutdown

--- cmd/port-service/main.go
+++ cmd/port-service/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/12ya/go-hexagonal-arch/internal/config"
 	"github.com/12ya/go-hexagonal-arch/internal/repository/inmemory"
@@ -36,10 +41,25 @@ func run() error {
 		Handler: router,
 	}
 
+	stopped := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("http server Shutdown Error: %v", err)
+		}
+		close(stopped)
+	}()
+
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatalf("http server ListenAndServe Error: %v", err)
 	}
 
+	<-stopped
 	log.Printf("Bye!")
 
 	return nil
